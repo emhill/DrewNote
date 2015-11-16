5246e8be301a2c8584b80894597b7a6de246a43e
Chris Thurber <cthurber@me.com>
2015-11-16 09:37:33 -0500

    Updated interface to Note objects, added missing methods

--- src/edu/drew/note/NoteCollection.java
+++ src/edu/drew/note/NoteCollection.java
@@ -2,17 +2,25 @@ package edu.drew.note;
 
 public interface NoteCollection<T> {
 
-//Checks to see whether the collection is empty.
-//returns true if it is, and false if otherwise.
-public boolean isEmpty();
+	//Adds new entry to the collection.
+	public boolean add(Note newNote);
+	
+	// Removes a Note and returns it
+	public long remove();
+	
+	// Removes a specific note
+	public boolean remove(long ID);
+	
+	//Checks to see whether the collection is empty.
+	//returns true if it is, and false if otherwise.
+	public boolean isEmpty();
+	
+	//Returns the size of the note collection.
+	public int getSize();
+	
+	//sees whether anEntry is in the collection.
+	public boolean contains(Note note);
 
-//Returns the size of the note collection.
-public int size();
-
-//sees whether anEntry is in the collection.
-public boolean contains(T anEntry);
-//Adds new entry to the collection.
-public boolean add(T newEntry);
-//
-public T [] toArray(T[] array);
+	// Returns an array of ...? 
+	public Note[] toArray(Note[] array);
 }
