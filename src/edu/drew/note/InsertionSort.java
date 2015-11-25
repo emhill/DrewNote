54d1b7f741d8bf7378ede4f57b560c05903c690e
Stephanie D'Andrea <sdandrea@drew.edu>
2015-11-25 17:12:33 -0500

    insertion sort pseudocode

---
+++ src/edu/drew/note/InsertionSort.java
@@ -0,0 +1,19 @@
+package edu.drew.note;
+
+//public class insertion_sort {
+
+/*
+	// pseudocode:
+	 * 
+	 * 
+	public static void insertion_code (int [] a) {
+		for j = 2 to A.length; //loop through each element
+				key = A[j]; //temporary variable used to hold the element we are comparing
+				i = j - 1; //look at number right in front of the key 
+				while (i > 0 && A[i] > key) //if element is greater than the key
+					A[i+1] = A[i] //move element into open spot behind it 
+					i = i - 1 //reset i to 0
+				A[i+1] = key
+	}
+}
+*/
