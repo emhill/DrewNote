c3f7db7c252206669f18d6494df2724bf89a116f
Stephanie D'Andrea <sdandrea@drew.edu>
2015-12-10 14:46:13 -0500

    now fully correct (off by one error)

--- src/edu/drew/note/InsertionSort.java
+++ src/edu/drew/note/InsertionSort.java
@@ -1,22 +1,32 @@
 package edu.drew.note;
 
-public class InsertionSort implements SortInterface {  //find the least and bring it to the beginning of the array 
+public class InsertionSort implements SortInterface { //find least # & bring to start of array
 
-	//sorts the array entries a[first] through a[last] iteratively
 	public Note[] sort(Note[] s) {
-		for (int j = s.length-1; j < 0; j--) {
+		for (int j = 1; j < s.length; j++) {
 			Note temp = s[j]; 
 			int i = j-1; 
-			while (temp.getID() < s[i].getID()) { //goes thru the array 
-				s[i+1] = s[i]; //overriding the position of the element of s (which is the last element)
-				i--; //indexes within the array
+			while ((i >= 0) && (temp.getID() < s[i].getID()) ) { //goes thru array 
+				s[i+1] = s[i]; //overriding position of element of s (which is last element)
+				i--; //indexes within array
 			}
-			//what happens when temp.getID() > s[i].getID()
-			s[i] = temp;
+			//what happens when temp.getID() < s[i].getID()
+			s[i+1] = temp;
 		}
 		return s;
 	}
+
+	public static void main(String[] args) {
+		Note a = new Note(); 
+		Note b = new Note(); 
+		Note c = new Note(); 
+		Note[] n = {c, b, a}; 
+
+		InsertionSort is = new InsertionSort();
+
+		// swap first & last
+		is.sort(n); 
+		for (int i = 0; i < n.length; i++)
+			System.out.println(n[i]); 
+	}
 }
-
-
-
